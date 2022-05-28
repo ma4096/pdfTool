4206de8ece111a8307b2a924ef1e14e70c3fbb7c
ma4096 <derbauermlg@gmail.com>
2022-05-28 20:15:23 +0200

    First Commit

---
+++ README.txt
@@ -0,0 +1,12 @@
+pdfTool is a basic tool to edit pdf documents.
+I started it to give me a free alternative to paid or online services, with which I am not comfortable with because of sensitive documents.
+This tool is most likely obsolete, but I wanted to make one myself :)
+
+It is based on the Python library PyPdf4.
+Their Github: https://github.com/claird/PyPDF4
+
+I run it in Python 3.9, it should be compatible with all Versions, which are compatible with PyPdf4.
+
+Common errors/tips:
+	When copying paths of files from Windows Explorer, they come with "[path]", the " have to be removed.
+	Paths can be either absolute or relative to the Python File.
---
+++ pdfTool.py
@@ -0,0 +1,88 @@
+from PyPDF4 import PdfFileReader, PdfFileWriter
+
+# Overwrite one page from basePdf with newPdf and safe it as pdf
+# Made for inserting single scanned pages into generated pdf for PP1, 5.2022
+
+header = """PDF Tool based on PyPDF4
+"""
+safePath = "C:/Users/derba/Documents/Jade_HS/PP1/test.pdf"
+
+def overwriteAt(writer):
+    print("Overwrite mode selected")
+    newPdfPath = input("Path of PDF to be inserted: ").replace("\\","/")
+    nPage = int(input("Page on which insertion should start: "))
+       
+    baseReader = PdfFileReader(basePdfPath)
+    newReader = PdfFileReader(newPdfPath)
+    if nPage > baseReader.getNumPages():
+        print("Error: Call wants to overwrite pages after ending of base")
+    else:
+        nrp = newReader.getNumPages()
+        
+        for page in range(nPage):
+            writer.addPage(baseReader.getPage(page))
+        for newPage in range(nrp):
+            writer.addPage(newReader.getPage(newPage))
+        for page in range(nPage+nrp, baseReader.getNumPages()):
+            writer.addPage(baseReader.getPage(page))
+            
+        with open(safePath, 'wb') as out:
+            writer.write(out)
+        print(f"Successfully overwritten pages {nPage}-{nPage+nrp}")
+        
+def appendEnd(writer):
+    print("Append mode selected")
+    seconPdfPath = input("Second pdf path: ").replace("\\","/")
+    
+    firstReader = PdfFileReader(basePdfPath)
+    seconReader = PdfFileReader(seconPdfPath)
+    for page in range(firstReader.getNumPages()):
+        writer.addPage(firstReader.getPage(page))
+    for page in range(seconReader.getNumPages()):
+        writer.addPage(seconReader.getPage(page))
+    with open(safePath, 'wb') as out:
+        writer.write(out)
+    print("Successfully appended")
+    
+def extract(writer):
+    print("Extraction mode selected")
+    firstPage = int(input("Page number of starting page: ")) - 1 #-1 because index
+    lastPage = int(input("Page number of the last page: ")) # no -1 because range func
+    
+    reader = PdfFileReader(basePdfPath)
+    if ((firstPage <= lastPage) and (lastPage <= reader.getNumPages())):
+        for page in range(firstPage, lastPage):
+            writer.addPage(reader.getPage(page))
+        with open(safePath, 'wb') as out:
+            writer.write(out)
+        print("Successfully extracted")
+    else:
+        print("Error: Most likely faulty page numbers")
+        
+if __name__ == "__main__":    
+    print(header)
+    modes = {
+        "o": ["overwrite pages from one pdf with another pdf", overwriteAt],
+        "a": ["append one pdf to another", appendEnd],
+        "e": ["extract pages from one pdf", extract],
+        "x": ["exit the program"]
+        }
+    while True:
+        print("mode\tdescription")
+        for mode in modes:
+            print(mode + "\t\t" + modes[mode][0])
+        selected_mode = input("Select mode: ")
+        if selected_mode == "x":
+            break
+        writerPdf = PdfFileWriter()
+        basePdfPath = input("Path of the base PDF: ").replace("\\","/")
+        safePath = input("Path to which the result will be saved: ").replace("\\","/")
+        try:
+            modes[selected_mode][1](writerPdf)
+        except OSError as err:
+            print("OS error: {0}".format(err))
+        except:
+            print("\nAN ERROR OCURRED. Returning to selection.\n")
+        
+    print("Program closed")
+    
